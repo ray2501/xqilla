e448fbd118539f245d693c985db0a941a477cace
Danilo Chang <ray2501@gmail.com>
2017-09-20 08:31:17 +0800

    Add build script

---
+++ build.tcl
@@ -0,0 +1,21 @@
+#!/usr/bin/tclsh
+
+set arch "x86_64"
+set base "XQilla-2.3.3"
+set fileurl "https://sourceforge.net/projects/xqilla/files/XQilla-2.3.3.tar.gz"
+
+set var [list wget $fileurl -O $base.tar.gz]
+exec >@stdout 2>@stderr {*}$var
+
+if {[file exists build]} {
+    file delete -force build
+}
+
+file mkdir build/BUILD build/RPMS build/SOURCES build/SPECS build/SRPMS
+file copy -force $base.tar.gz build/SOURCES
+
+set buildit [list rpmbuild --target $arch --define "_topdir [pwd]/build" -bb xqilla.spec]
+exec >@stdout 2>@stderr {*}$buildit
+
+# Remove our source code
+file delete $base.tar.gz
